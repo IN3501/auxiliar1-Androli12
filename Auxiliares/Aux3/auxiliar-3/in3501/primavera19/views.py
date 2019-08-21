8f37431cee486f1200340c10a4fe8bebefb1899d
Androli12 <andresolivares1998@gmail.com>
2019-08-21 13:59:01 -0400

    cambios

---
+++ Auxiliares/Aux3/auxiliar-3/in3501/primavera19/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+	return render(request, 'home.html')
+
+def inputs(request):
+	return render(request, 'inputs.html')
+
+def registro(request):
+	return render(request, 'registro.html')
